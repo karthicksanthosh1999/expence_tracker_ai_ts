5218d9c9bee4b895d35bf327b297a02707b2d9dd
karthicksanthosh <karthick.santhosh1999@gmail.com>
2025-12-30 22:46:53 +0530

    Completed the transaction filter

--- app/(main)/account/_conponents/transaction-table.tsx
+++ app/(main)/account/_conponents/transaction-table.tsx
@@ -11,6 +11,14 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   Table,
   TableBody,
@@ -29,7 +37,18 @@ import { categoryColors } from "@/data/categories";
 import { Transactions } from "@/lib/generated/prisma";
 import { RecurringInterval } from "@/lib/generated/prisma/enums";
 import { format } from "date-fns";
-import { Clock, MoreHorizontal, RefreshCcw } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronUp,
+  Clock,
+  MoreHorizontal,
+  RefreshCcw,
+  Search,
+  Trash,
+  X,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
 
 type TTransaction = {
   id: string;
@@ -50,10 +69,89 @@ type TTransaction = {
 type TProps = {
   transaction: TTransaction[];
 };
-const TransactionTable = ({ transaction }: TProps) => {
-  const filterAndSortedTransactions = transaction;
 
-  const handleSort = (params: string) => {};
+const TransactionTable = ({ transaction }: TProps) => {
+  const router = useRouter();
+
+  const [selectedIds, setSelectedIds] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [typeFilter, setTypeFilter] = useState("");
+  const [recurringFilter, setRecurringFilter] = useState("");
+
+  const [sortConfig, setSortConfig] = useState({
+    field: "date",
+    direction: "desc",
+  });
+
+  const filterAndSortedTransactions = useMemo(() => {
+    let result = [...transaction];
+
+    if (searchTerm) {
+      const searchLower = searchTerm.toLowerCase();
+      result = result.filter((transaction) =>
+        transaction.description?.toLocaleLowerCase().includes(searchLower)
+      );
+    }
+
+    if (recurringFilter) {
+      result = result.filter((transaction) => {
+        if (recurringFilter === "recurring") return transaction.isRecurring;
+        return !transaction.isRecurring;
+      });
+    }
+
+    if (typeFilter) {
+      result = result.filter((transaction) => transaction.type === typeFilter);
+    }
+
+    result.sort((a, b) => {
+      let comparison = 0;
+
+      switch (sortConfig.field) {
+        case "date":
+          comparison = new Date(a.date).getTime() - new Date(b.date).getTime();
+          break;
+        case "amount":
+          comparison = a.amount - b.amount;
+          break;
+        case "category":
+          comparison = (a.category ?? "").localeCompare(b.category ?? "");
+          break;
+        default:
+          comparison = 0;
+      }
+
+      return sortConfig.direction === "asc" ? comparison : -comparison;
+    });
+
+    return result;
+  }, [searchTerm, transaction, typeFilter, recurringFilter, sortConfig]);
+
+  const handleSort = (field: string) => {
+    setSortConfig((current) => ({
+      field,
+      direction:
+        current.field === field && current.direction === "asc" ? "desc" : "asc",
+    }));
+  };
+
+  const handelSelect = (id: string) => {
+    setSelectedIds((current) =>
+      current.includes(id)
+        ? current.filter((item) => item !== id)
+        : [...current, id]
+    );
+  };
+
+  const handleSelectAll = () => {
+    setSelectedIds((current) =>
+      current.length === filterAndSortedTransactions.length
+        ? []
+        : filterAndSortedTransactions.map((t) => t.id)
+    );
+  };
+
+  const handleBulkDelete = () => {};
 
   const RECURRING_INTERVALS = {
     DAILY: "DAILY",
@@ -62,30 +160,123 @@ const TransactionTable = ({ transaction }: TProps) => {
     YEARLY: "YEARLY",
   };
 
+  const handleClearFilter = () => {
+    setSearchTerm("");
+    setTypeFilter("");
+    setRecurringFilter("");
+    setSelectedIds([]);
+  };
+
   return (
     <div className="space-y-4">
+      {/* FILTER */}
+      <div className="flex flex-col sm:flex-row gap-4">
+        <div className="relative flex-1">
+          <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            className="pl-8"
+            placeholder="Search transactions..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <div className="flex gap-2">
+          <Select value={typeFilter} onValueChange={setTypeFilter}>
+            <SelectTrigger className="w-32">
+              <SelectValue placeholder="All Types" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="INCOME">Income</SelectItem>
+              <SelectItem value="EXPENSE">Expense</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select
+            value={recurringFilter}
+            onValueChange={(value) => setRecurringFilter(value)}>
+            <SelectTrigger className="w-32.5">
+              <SelectValue placeholder="All Transactions" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="recurring">Recurring Only</SelectItem>
+              <SelectItem value="non-recurring">Non-Recurring Only</SelectItem>
+            </SelectContent>
+          </Select>
+          {selectedIds.length > 0 && (
+            <div className="flex items-center gap-2">
+              <Button
+                variant={"destructive"}
+                size={"sm"}
+                onClick={handleBulkDelete}>
+                <Trash className="h-4 w-4 mr-2" />
+                Delete Selected ({selectedIds.length})
+              </Button>
+            </div>
+          )}
+          {(searchTerm || typeFilter || recurringFilter) && (
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              onClick={handleClearFilter}
+              title="Clear">
+              <X className="h-4 w-5" />
+            </Button>
+          )}
+        </div>
+      </div>
+      {/* TRANSACTION */}
       <div className="rounded-md border">
         <Table>
           <TableHeader>
             <TableRow>
               <TableHead className="w-12.5">
-                <Checkbox className="cursor-pointer" />
+                <Checkbox
+                  className="cursor-pointer"
+                  onCheckedChange={handleSelectAll}
+                  checked={
+                    selectedIds.length === filterAndSortedTransactions.length &&
+                    filterAndSortedTransactions.length > 0
+                  }
+                />
               </TableHead>
               <TableHead
                 className="cursor-pointer"
                 onClick={() => handleSort("date")}>
-                <div className="flex items-center">Date</div>
+                <div className="flex items-center">
+                  Date{" "}
+                  {sortConfig.field === "date" &&
+                    (sortConfig.direction === "asc" ? (
+                      <ChevronUp className="ml-1 h-4 w-4" />
+                    ) : (
+                      <ChevronDown className="ml-1 h-4 w-4" />
+                    ))}
+                </div>
               </TableHead>
               <TableHead>Description</TableHead>
               <TableHead
                 className="cursor-pointer"
                 onClick={() => handleSort("category")}>
-                <div className="flex items-center ">Category</div>
+                <div className="flex items-center ">
+                  Category
+                  {sortConfig.field === "category" &&
+                    (sortConfig.direction === "asc" ? (
+                      <ChevronUp className="ml-1 h-4 w-4" />
+                    ) : (
+                      <ChevronDown className="ml-1 h-4 w-4" />
+                    ))}
+                </div>
               </TableHead>
               <TableHead
                 className="cursor-pointer"
                 onClick={() => handleSort("amount")}>
-                <div className="flex items-center justify-end">Amount</div>
+                <div className="flex items-center justify-end">
+                  Amount
+                  {sortConfig.field === "amount" &&
+                    (sortConfig.direction === "asc" ? (
+                      <ChevronUp className="ml-1 h-4 w-4" />
+                    ) : (
+                      <ChevronDown className="ml-1 h-4 w-4" />
+                    ))}
+                </div>
               </TableHead>
               <TableHead>Recurring</TableHead>
               <TableHead className="w-12.5" />
@@ -104,7 +295,11 @@ const TransactionTable = ({ transaction }: TProps) => {
               filterAndSortedTransactions.map((item) => (
                 <TableRow key={item.id}>
                   <TableCell>
-                    <Checkbox className="cursor-pointer" />
+                    <Checkbox
+                      className="cursor-pointer"
+                      onCheckedChange={() => handelSelect(item.id)}
+                      checked={selectedIds.includes(item.id)}
+                    />
                   </TableCell>
                   <TableCell>{format(new Date(item.date), "PP")}</TableCell>
                   <TableCell>{item.description}</TableCell>
@@ -163,10 +358,17 @@ const TransactionTable = ({ transaction }: TProps) => {
                         </Button>
                       </DropdownMenuTrigger>
                       <DropdownMenuContent>
-                        <DropdownMenuItem className="cursor-pointer">
+                        <DropdownMenuItem
+                          onClick={() =>
+                            router.push(`/transaction/create?edit=${item.id}`)
+                          }
+                          className="cursor-pointer">
                           Edit
                         </DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer">
+                        <DropdownMenuItem
+                          className="cursor-pointer text-destructive"
+                          // onClick={() => deleteFn([item.id])}
+                        >
                           Delete
                         </DropdownMenuItem>
                       </DropdownMenuContent>
